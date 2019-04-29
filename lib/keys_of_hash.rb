f2607911cf4852291a444b94867c8155776e3d62
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-04-29 23:36:01 +0800

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array=[]
+    self.each do |animal,place|
+      #self is where we put the hash
+      #hash can be found in the spec file
+      arguments.each do |i|
+        array.push(animal) if i==place
+      end
+    end
+    array
   end
 end
